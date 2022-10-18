95a24391d2a31e793d469a8937250c296b2fc1f5
Marcelo Rosales <mgrt99@gmail.com>
2022-10-18 16:48:08 -0600

    agregar colores a los botones

--- Contador.js
+++ Contador.js
@@ -4,12 +4,29 @@ document.getElementById("contador").innerText = contador;
 function Incrementar(){
     contador++;
     document.getElementById("contador").innerText = contador;
+    color();
 }
 function Decrementar(){
     contador--;
     document.getElementById("contador").innerText = contador;
+    color();
 }
 function Reiniciar(){
     contador=0;
     document.getElementById("contador").innerText = contador;
+    color();
+}
+function color(){
+    if(document.getElementById("contador").innerText==0)
+    {
+        document.getElementById("contador").style.color = "blue";
+
+    }
+    else if(document.getElementById("contador").innerText>0)
+    {
+        document.getElementById("contador").style.color = "green";
+    }
+    else {
+        document.getElementById("contador").style.color = "red";
+    }
 }
